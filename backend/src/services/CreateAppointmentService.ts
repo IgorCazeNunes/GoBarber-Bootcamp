a10cdf949770e28c95115eea559a935dca4f6e40
Crummorn <Igorcaze@gmail.com>
2021-01-03 11:58:41 -0300

    Chore: Adaptação de todos os metodos existentes para utilizar o TypeORM.

--- backend/src/services/CreateAppointmentService.ts
+++ backend/src/services/CreateAppointmentService.ts
@@ -1,4 +1,5 @@
 import { startOfHour } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
 
 import Appointment from "../models/Appointment";
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
@@ -10,27 +11,27 @@ interface RequestDTO {
 
 class CreateAppointmentService {
 
-    private appointmentsRepository: AppointmentsRepository;
+    public async execute({ provider, date }: RequestDTO): Promise<Appointment> {
 
-    constructor(appointmentsRepository: AppointmentsRepository) {
-        this.appointmentsRepository = appointmentsRepository;
-    }
+        const appointmentsRepository = getCustomRepository(AppointmentsRepository);
 
-    public execute({ provider, date }: RequestDTO): Appointment {
         const appointmentDate = startOfHour(date);
 
-        const findAppointmentInSameDate = this.appointmentsRepository.findByDate(appointmentDate);
+        const findAppointmentInSameDate = await appointmentsRepository.findByDate(appointmentDate);
 
         if (findAppointmentInSameDate) {
             throw Error("This appointment is already booked");
         }
 
-        const appointment = this.appointmentsRepository.create({
+        const appointment = appointmentsRepository.create({
             provider,
             date: appointmentDate,
         });
 
+        await appointmentsRepository.save(appointment);
+
         return appointment;
+
     }
 
 }
