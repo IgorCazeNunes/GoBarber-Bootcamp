16c663abacf7468c7627615acfe994da14b73982
Crummorn <Igorcaze@gmail.com>
2021-01-03 11:52:37 -0300

    Fix: Removido bug da não geração de uuid de appointments.

--- backend/src/database/migrations/1609682207614-CreateAppointments.ts
+++ backend/src/database/migrations/1609682207614-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1609682207614 implements MigrationInterfa
                         type: 'varchar',
                         isPrimary: true,
                         generationStrategy: 'uuid',
+                        default: 'uuid_generate_v4()',
                     },
                     {
                         name: 'provider',
