3432ef76e676f2df2a12b33b717ae8b5441e0776
Crummorn <Igorcaze@gmail.com>
2021-01-04 23:51:57 -0300

    Chore: Finalizado atualização de avatar.

--- backend/src/routes/users.routes.ts
+++ backend/src/routes/users.routes.ts
@@ -3,6 +3,8 @@ import multer from 'multer';
 import uploadConfig from '../config/upload';
 
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
+
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const usersRouter = Router();
@@ -37,9 +39,29 @@ usersRouter.post('/', async (request, response) => {
 });
 
 usersRouter.patch('/avatar', ensureAuthenticated, upload.single('avatar'), async (request, response) => {
-    console.log(request.file);
 
-    return response.json({ ok: true });
+    try {
+        const updateUserAvatar = new UpdateUserAvatarService();
+
+        const user = await updateUserAvatar.execute({
+            user_id: request.user.id,
+            avatarFileName: request.file.filename,
+        });
+
+
+        const userWithoutPassword = {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            created_at: user.created_at,
+            updated_at: user.updated_at,
+        };
+
+        return response.json({ userWithoutPassword });
+    } catch (err) {
+        return response.status(400).json({ error: err.message });
+    }
+
 });
 
 export default usersRouter;
