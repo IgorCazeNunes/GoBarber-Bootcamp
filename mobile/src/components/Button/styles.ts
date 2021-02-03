b75d5f541a76710159cdb728ba70d5e7404fe9ac
Crummorn <Igorcaze@gmail.com>
2021-02-03 12:56:32 -0300

    M-Chore: Finalizado estruturação do Component Button.

--- mobile/src/components/Button/styles.ts
+++ mobile/src/components/Button/styles.ts
@@ -1,3 +1,19 @@
 import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
 
-export const Container = styled.View``;
+export const Container = styled(RectButton)`
+  align-items: center;
+  justify-content: center;
+
+  width: 100%;
+  height: 60px;
+
+  background: #ff9000;
+  border-radius: 10px;
+`;
+
+export const ButtonText = styled.Text`
+  color: #312e38;
+  font-size: 16px;
+  font-family: 'RobotoSlab-Medium';
+`;
