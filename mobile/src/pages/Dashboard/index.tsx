7ce2093fe3771a8e200c22a8f847a851b1ca8980
Crummorn <Igorcaze@gmail.com>
2021-02-12 19:23:34 -0300

    M-Feat: Add botão de logout a Page Dashboard.

--- mobile/src/pages/Dashboard/index.tsx
+++ mobile/src/pages/Dashboard/index.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import { View } from 'react-native';
+import Button from '../../components/Button';
+import { useAuth } from '../../hooks/auth';
 
-const Dashboard: React.FC = () => <View />;
+const Dashboard: React.FC = () => {
+  const { signOut } = useAuth();
+
+  return (
+    <View>
+      <Button onPress={signOut}>Sair</Button>
+    </View>
+  );
+};
 
 export default Dashboard;
