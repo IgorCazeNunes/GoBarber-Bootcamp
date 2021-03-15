b173ce522e4cf9b35dcafb027e5abaa2c5a37391
Crummorn <igorcaze@gmail.com>
2021-03-15 11:25:27 -0300

    F-Feat: Add Estrutura/Estilização do Header ao Dashboard. (Estatico)

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,5 +1,36 @@
 import React from 'react';
+import { FiPower } from 'react-icons/fi';
 
-const Dashboard: React.FC = () => <h1>Dashboard</h1>;
+import logoImg from '../../assets/logo.svg';
+
+import { Container, Header, HeaderContent, Profile } from './styles';
+
+const Dashboard: React.FC = () => {
+  return (
+    <Container>
+      <Header>
+        <HeaderContent>
+          <img src={logoImg} alt="GoBarber" />
+
+          <Profile>
+            <img
+              src="https://github.com/IgorCazeNunes.png"
+              alt="Igor Cazé Nunes"
+            />
+
+            <div>
+              <span>Bem-vindo</span>
+              <strong>Igor Cazé Nunes</strong>
+            </div>
+          </Profile>
+
+          <button type="button">
+            <FiPower />
+          </button>
+        </HeaderContent>
+      </Header>
+    </Container>
+  );
+};
 
 export default Dashboard;
