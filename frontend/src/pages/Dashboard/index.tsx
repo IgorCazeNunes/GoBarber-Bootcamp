0176caf0a7666d22267e55bf5fa246cace70883d
Crummorn <igorcaze@gmail.com>
2021-03-15 12:26:57 -0300

    F-Chore: Add Estrutura e Estilização da listagem de agendamentos no Dashboard.

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
-import { FiPower } from 'react-icons/fi';
+import { FiClock, FiPower } from 'react-icons/fi';
 
 import logoImg from '../../assets/logo.svg';
 import { useAuth } from '../../hooks/auth';
 
-import { Container, Header, HeaderContent, Profile } from './styles';
+import {
+  Container,
+  Content,
+  Header,
+  HeaderContent,
+  Profile,
+  Schedule,
+  NextAppointment,
+  Calendar,
+} from './styles';
 
 const Dashboard: React.FC = () => {
   const { signOut, user } = useAuth();
@@ -29,6 +38,66 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+
+          <p>
+            <span>Hoje</span>
+            <span>Dia 06</span>
+            <span>Segunda-feira</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+
+            <div>
+              <img
+                src="https://github.com/IgorCazeNunes.png"
+                alt="Igor Cazé Nunes"
+              />
+
+              <strong>Igor Cazé Nunes</strong>
+
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+
+            <div>
+              <img
+                src="https://github.com/IgorCazeNunes.png"
+                alt="Igor Cazé Nunes"
+              />
+
+              <strong>Igor Cazé Nunes</strong>
+
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+
+            <div>
+              <img
+                src="https://github.com/IgorCazeNunes.png"
+                alt="Igor Cazé Nunes"
+              />
+
+              <strong>Igor Cazé Nunes</strong>
+
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+        </Schedule>
+
+        <Calendar />
+      </Content>
     </Container>
   );
 };
--- frontend/src/pages/Dashboard/styles.ts
+++ frontend/src/pages/Dashboard/styles.ts
@@ -2,6 +2,14 @@ import styled from 'styled-components';
 
 export const Container = styled.div``;
 
+export const Content = styled.main`
+  display: flex;
+
+  max-width: 1120px;
+
+  margin: 64px auto;
+`;
+
 export const Header = styled.header`
   padding: 32px 0;
   background: #28262e;
@@ -65,4 +73,108 @@ export const Profile = styled.div`
       color: #ff9000;
     }
   }
+`;
+
+export const Schedule = styled.div`
+  flex: 1;
+
+  margin-right: 120px;
+
+  h1 {
+    font-size: 36px;
+  }
+
+  p {
+    display: flex;
+    align-items: center;
+
+    margin-top: 8px;
+
+    color: #ff9000;
+    font-weight: 500;
+
+    span {
+      display: flex;
+      align-items: center;
+    }
+
+    span + span::before {
+      content: '';
+
+      width: 1px;
+      height: 12px;
+
+      margin: 0 8px;
+
+      background: #ff9000;
+    }
+  }
+`;
+
+export const NextAppointment = styled.div`
+  margin-top: 64px;
+
+  > strong {
+    color: #999591;
+    font-size: 20px;
+    font-weight: 400;
+  }
+
+  div {
+    display: flex;
+    align-items: center;
+
+    padding: 16px 24px;
+    margin-top: 24px;
+
+    position: relative;
+    background: #3e3b47;
+    border-radius: 10px;
+
+    &::before {
+      content: '';
+
+      height: 80%;
+      width: 1px;
+
+      left: 0;
+      top: 10%;
+
+      position: absolute;
+      background: #ff9000;
+    }
+
+    img {
+      width: 80px;
+      height: 80px;
+
+      background: #ff9000;
+      border-radius: 50%;
+    }
+
+    strong {
+      margin-left: 24px;
+
+      color: #fff;
+    }
+
+    span {
+      display: flex;
+      align-items: center;
+
+      margin-left: auto;
+
+      color: #999591;
+
+      svg {
+        margin-right: 8px;
+
+        color: #ff9000;
+      }
+    }
+  }
+`;
+
+export const Calendar = styled.aside`
+  width: 380px;
 `;
