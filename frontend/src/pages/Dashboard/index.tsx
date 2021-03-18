2b17bf18b31a1bc21a7ff0c787cb765121b214c4
Crummorn <igorcaze@gmail.com>
2021-03-18 14:02:49 -0300

    F-Feat: Adicionado rota para Profile.

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -4,6 +4,7 @@ import ptBR from 'date-fns/locale/pt-BR';
 import DayPicker, { DayModifiers } from 'react-day-picker';
 import { FiClock, FiPower } from 'react-icons/fi';
 
+import { Link } from 'react-router-dom';
 import { useAuth } from '../../hooks/auth';
 
 import api from '../../services/api';
@@ -148,7 +149,9 @@ const Dashboard: React.FC = () => {
 
             <div>
               <span>Bem-vindo</span>
-              <strong>{user.name}</strong>
+              <Link to="/profile">
+                <strong>{user.name}</strong>
+              </Link>
             </div>
           </Profile>
 
